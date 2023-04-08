1cba5ddab126ad2bd2dd353c8468ec2bd9776f6d
Lucas Bragança Gonçalves <100163176+lubragon@users.noreply.github.com>
2023-04-07 23:11:40 -0300

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -5,10 +5,10 @@
  */
 
 SELECT 
-	o.fname    	AS nome,
-        o.lname     	AS sobrenome,
-        b.name 		AS negocio,
-        c.city 		AS cidade
+	o.fname    			AS nome,
+        o.lname     			AS sobrenome,
+        b.name 				AS negocio,
+        c.city 				AS cidade
 FROM business b
 INNER JOIN officer o ON (o.cust_id = b.cust_id)
 INNER JOIN customer c ON (c.cust_id = b.cust_id);
@@ -20,8 +20,8 @@ INNER JOIN customer c ON (c.cust_id = b.cust_id);
 */
 
 SELECT DISTINCT
-		'FIS' 				AS Type,
-		CONCAT(i.fname, ' ', i.lname) 	AS Name
+	'FIS' 				AS Type,
+	CONCAT(i.fname, ' ', i.lname) 	AS Name
 FROM customer c
 INNER JOIN individual i ON(c.cust_id = i.cust_id)
 INNER JOIN account a ON(c.cust_id = a.cust_id)
@@ -45,17 +45,17 @@ WHERE bra.city != c.city;
 	Escreva uma consulta que retorne os nome de todos os funcionários com, se for o caso, os números de
 	transações por ano envolvendo as contas que eles abriram (usando open_emp_id). Ordene os resultados
 	por ordem alfabética, e depois por ano (do mais antigo para o mais recente)
-
 */
 
 SELECT 	
-	CONCAT(e.fname, ' ', e.lname) 			AS Name,
-	COUNT(t.txn_id) 				AS qtTrans
-FROM account a
-RIGHT OUTER JOIN employee e ON(e.emp_id = a.open_emp_id)
+	CONCAT(e.fname, ' ', e.lname) 		AS Nome,
+	COUNT(t.txn_id) 			AS qtTrans,
+	COALESCE(YEAR(t.txn_date))	 	AS Ano
+FROM employee e
+LEFT JOIN account a ON(e.emp_id = a.open_emp_id)
 LEFT JOIN transaction t ON(a.account_id = t.account_id)
-GROUP BY Name
-ORDER BY Name ASC, t.txn_date;
+GROUP BY Nome, Ano
+ORDER BY Nome ASC, Ano DESC;
 
 
 /*
@@ -63,14 +63,13 @@ ORDER BY Name ASC, t.txn_date;
 	Escreva uma consulta que retorne os identificadores de contas com maior saldo de dinheiro por agência,
 	juntamente com os nomes dos titulares (nome da empresa ou nome e sobrenome da pessoa física) e os
 	nomes dessas agências.
-
 */
 SELECT
-	'FIS'                    AS Type,
-	account_id 	         AS ID,
-	CONCAT(fname,' ', lname) AS Name,
-	bra.name 	       	 AS BranchName,		
-	a.avail_balance 	 AS Balance                   
+	'FIS'                    	AS Type,
+	account_id 	        	AS ID,
+	CONCAT(fname,' ', lname) 	AS Name,
+	bra.name 	       	 	AS BranchName,		
+	a.avail_balance 	 	AS Balance                   
 FROM account a	 
 INNER JOIN individual USING (cust_id)
 INNER JOIN branch bra ON (open_branch_id = branch_id)
@@ -104,6 +103,8 @@ INNER JOIN (
 	Questão 5:
 	Escreva de novo as consultas 2. e 4. utilizando uma visualização (CREATE VIEW).
 */
+
+// 2
 CREATE VIEW ClientType 	AS
 SELECT DISTINCT
 		'FIS' 				AS Type,
@@ -125,7 +126,7 @@ WHERE bra.city != c.city;
 
 
 
-
+//4
 CREATE VIEW maxAvail AS
 SELECT
 	'FIS'                    AS Type,
